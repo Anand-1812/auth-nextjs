8e11785f68b240bc430318b29ce5806e8282bb4c
Anand-1812 <anand.work2005@gmail.com>
2025-10-02 13:15:31 +0530

    initial commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="text-center text-white text-2xl">login</h1>
+    </div>
+  )
+}
