beb96e0024b8d02dc40f079ed5b1243e79541dc3
Anand-1812 <anand.work2005@gmail.com>
2025-10-03 22:57:06 +0530

    styling the login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,9 @@
 export default function LoginPage() {
   return (
-    <div className="flex justify-center items-center">
-      <h1 className="text-center text-white text-2xl">login</h1>
+    <div className="min-h-screen flex justify-center items-center bg-gradient-to-br from-green-100 via-white to-green-200 px-4">
+      <div className="w-full max-w-md">
+        <h1 className="text-3xl text-black">Login here</h1>
+      </div>
     </div>
   )
 }
