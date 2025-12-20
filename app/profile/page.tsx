f5cc9dea3eaeff19eb54f7caf1aada3df95a6e0d
Anand-1812 <anand.work2005@gmail.com>
2025-12-20 22:12:14 +0530

    feat: user login and redirect to profile page

---
+++ app/profile/page.tsx
@@ -0,0 +1,9 @@
+const Profile = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center  p-3 rounded-xl">
+      <h1 className="text-3xl text-indigo-600">Profile Page :)</h1>
+    </div>
+  )
+}
+
+export default Profile
